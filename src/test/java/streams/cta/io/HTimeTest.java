69a685a80e9c2955f934883e33aa00784e6ab833
Alexey Egorov <aegorov87@gmail.com>
2015-08-07 17:20:52 +0200

    add test that fails now and should be implemented (easy one)

---
+++ src/test/java/streams/cta/io/HTimeTest.java
@@ -0,0 +1,26 @@
+package streams.cta.io;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by alexey on 07/08/15.
+ */
+public class HTimeTest {
+
+    @Test
+    public void testReadTime() throws Exception {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testResetTime() throws Exception {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testGetAsLocalDateTime() throws Exception {
+        assertEquals(0, 1);
+    }
+}
