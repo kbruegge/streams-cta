99c160f9935e2ac9ade3bbdfcb1f13d49298ab91
Alexey Egorov <aegorov87@gmail.com>
2015-08-14 10:35:34 +0200

    style

--- src/main/java/streams/cta/io/SyntheticEventStream.java
+++ src/main/java/streams/cta/io/SyntheticEventStream.java
@@ -1,9 +1,8 @@
 /**
- * 
+ *
  */
 package streams.cta.io;
 
-import java.nio.ByteBuffer;
 import java.time.LocalDateTime;
 import java.util.Random;
 
@@ -15,48 +14,54 @@ import streams.cta.CTATelescopeType;
 
 /**
  * @author chris, kai
- * 
  */
 public class SyntheticEventStream extends AbstractStream {
 
-	int numberOfPixels = 1855;
-	int numberOfSlices = 30;
+    int numberOfPixels = 1855;
+    int numberOfSlices = 30;
 
-	Random random = new Random();
+    Random random = new Random();
     byte[] randomBytes;
     short[][] data;
 
+    int counter;
+
 
     long eventId = 1;
-	int telescopeId = 0;
-	int[] brokenPixel = new int[]{2,12,333};
-	double[] gains = new double[numberOfPixels];
-	CTATelescope telescope = new CTATelescope(CTATelescopeType.LST, telescopeId, 0 ,0 ,0, brokenPixel, gains, gains  );
+    int telescopeId = 0;
+    int[] brokenPixel = new int[]{2, 12, 333};
+    double[] gains = new double[numberOfPixels];
+    CTATelescope telescope = new CTATelescope(CTATelescopeType.LST, telescopeId, 0, 0, 0, brokenPixel, gains, gains);
 
-	public double f(int x){
-        return 100*Math.exp(-0.04*Math.pow((x - (10 + 4*(random.nextDouble() - 0.5))), 2));
+    public double f(int x) {
+        return 100 * Math.exp(-0.04 * Math.pow((x - (10 + 4 * (random.nextDouble() - 0.5))), 2));
     }
 
-	/**
-	 * @see stream.io.AbstractStream#init()
-	 */
-	@Override
-	public void init() throws Exception {
-		super.init();
-	}
+    /**
+     * @see stream.io.AbstractStream#init()
+     */
+    @Override
+    public void init() throws Exception {
+        super.init();
+    }
 
-	/**
-	 * @see stream.io.AbstractStream#readNext()
-	 */
-	@Override
-	public Data readNext() throws Exception {
+    /**
+     * @see stream.io.AbstractStream#readNext()
+     */
+    @Override
+    public Data readNext() throws Exception {
+        if (counter++>3000){
+            System.out.println("sleep");
+            Thread.sleep(20000);
+            counter = 0;
+        }
+
         data = new short[numberOfPixels][numberOfSlices];
-
 
         for (int pixel = 0; pixel < numberOfPixels; pixel++) {
             for (int x = 0; x < numberOfSlices; x++) {
-                data[pixel][x] = (short) f(x);
-                data[pixel][x] += 10*random.nextGaussian();
+                //data[pixel][x] = (short) f(x);
+                data[pixel][x] += random.nextGaussian();
             }
 //            randomBytes = new byte[numberOfSlices*2];
 //            random.nextBytes(randomBytes);
@@ -64,13 +69,13 @@ public class SyntheticEventStream extends AbstractStream {
         }
 
 
-		Data item = DataFactory.create();
-		item.put("@raw_data", data);
-		item.put("@telescope", telescope);
-		item.put("@timestamp", LocalDateTime.now());
-		item.put("@source", this.getClass().getSimpleName());
+        Data item = DataFactory.create();
+        item.put("@raw_data", data);
+        item.put("@telescope", telescope);
+        item.put("@timestamp", LocalDateTime.now());
+        item.put("@source", this.getClass().getSimpleName());
 
         eventId++;
-		return item;
-	}
+        return item;
+    }
 }
