ca3aee7430faf89e34bad32c5f314cf63a195a8f
Alexey Egorov <aegorov87@gmail.com>
2015-08-20 11:42:25 +0200

    produce two different jars (one to deploy to storm and one to run locally)

---
+++ packageStormForDeploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [ "$1" == "deployRun" ] || [ $# -eq 0 ]; then
+    # package for deployment
+    mvn -P deploy package
+
+    # package for local start
+    mvn -P standalone package
+fi
+
+if [ "$1" == "run" ] || [ "$1" == "deployRun" ]; then
+    # start the deployment
+    java -jar -Dnimbus.host=localhost -Dstorm.jar=target/cta-tools-0.0.1-SNAPSHOT-storm-provided.jar target/cta-tools-0.0.1-SNAPSHOT-storm-compiled.jar src/test/resources/performance-synthetic-stream.xml
+fi
