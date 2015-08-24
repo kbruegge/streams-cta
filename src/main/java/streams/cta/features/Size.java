2d558b3f9dc6af128400587e49c76eb7e4bcf885
jens buß <jebuss@jenss-Mac-mini.local>
2015-08-24 13:40:57 +0200

    Implementation of a processor calculating the Hillas parameter Size

---
+++ src/main/java/streams/cta/features/Size.java
@@ -0,0 +1,30 @@
+package streams.cta.features;
+
+import stream.Data;
+import streams.cta.CTACleanedDataProcessor;
+import streams.cta.CTATelescope;
+import streams.hexmap.CameraPixel;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+
+/**
+ * Created by jebuss on 24.08.15.
+ */
+public class Size extends CTACleanedDataProcessor{
+
+    @Override
+    public Data process(Data input, CTATelescope telescope, LocalDateTime timeStamp, double[] photons,
+                        double[] arrivalTimes, HashSet<CameraPixel> showerPixel) {
+
+        double size = 0;
+
+
+        for (CameraPixel pix : showerPixel){
+            size += photons[pix.id];
+        }
+
+        input.put("size", size);
+        return input;
+    }
+}
