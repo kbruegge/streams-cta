f2dfcc2be5884154003d3cb2107b194c4b3b24ed
Kai Bruegge <kai.bruegge@udo.edu>
2015-08-27 18:33:46 +0200

    some minor xml changes

--- packageStormForDeploy.sh
+++ packageStormForDeploy.sh
@@ -12,3 +12,6 @@ if [ "$1" == "run" ] || [ "$1" == "deployRun" ]; then
     # start the deployment
     java -jar -Dnimbus.host=localhost -Dstorm.jar=target/cta-tools-0.0.1-SNAPSHOT-storm-provided.jar target/cta-tools-0.0.1-SNAPSHOT-storm-compiled.jar src/test/resources/performance-synthetic-stream.xml
 fi
+
+
+#java -jar -Dnimbus.host=129.217.160.98 -Dstorm.jar=target/cta-tools-0.0.1-SNAPSHOT-storm-provided.jar target/cta-tools-0.0.1-SNAPSHOT-storm-compiled.jar streams-processes/storm/subscribe.xml
