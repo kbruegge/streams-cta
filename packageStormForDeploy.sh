19540935efbe99e1e4e5dcf479ca1d9631049e03
Alexey Egorov <aegorov87@gmail.com>
2015-09-16 14:37:28 +0200

    added option to build with storm.deploy or storm.run

--- packageStormForDeploy.sh
+++ packageStormForDeploy.sh
@@ -5,23 +5,28 @@ process_path="streams-processes/zeromq/subscribe.xml"
 run=false
 build=false
 nimbus="localhost"
+stormmainclass="storm.deploy"
 
 # handle the argument options
 function USAGE {
-    echo "-p    <path to XML process file>"
-    echo "-n    <nimbus host ip adress>"
-    echo "-r    run the deployment process"
-    echo "-b    build new packages"
+    echo ""
+    echo "Usage:"
+    echo "-p    <path to XML process file>      if not defined default value will be used"
+    echo "-n    <nimbus host ip adress>         localhost as default"
+    echo "-m    <storm.deploy|storm.run>        define mainclass to deploy or run local cluster"
+    echo "-r                                    run the deployment process"
+    echo "-b                                    build new packages"
     exit 1
 }
 
-while getopts ":pn: :rb" optname
+while getopts ":pnm: :rb" optname
   do
     case "$optname" in
       "p") process_path=$OPTARG;;
       "r") run=true;;
       "b") build=true;;
       "n") nimbus=$OPTARG;;
+      "m") stormmainclass=$OPTARG;;
       "?") USAGE;;
       ":")
         echo "No argument value for option $OPTARG"
@@ -39,9 +44,12 @@ echo "Using following process as topology: $process_path"
 echo "Rebuild: $build"
 echo "Running: $run"
 echo "Nimbus host: $nimbus"
+echo "Storm mainclass: $stormmainclass"
 
 # stop and print the usage if both r and b were not set
-if [ !$run ] && [ !$build ]; then
+# or if mainclass is set to sth different then storm.{deploy,run}
+if ( ( [ $run == "false" ] ) && ( [ $build == "false" ] ) ) || \
+        ( [[ ! $stormmainclass =~ ^storm\.(deploy|run)$ ]] ) ; then
     USAGE
 fi
 
@@ -51,15 +59,15 @@ if $build; then
     mvn -P deploy package
 
     # package for local start
-    mvn -P standalone package
+    mvn -Dstorm.mainclass=${stormmainclass} -P standalone package
 fi
 
 # run the deployment process
 if $run; then
     # start the deployment
     java -jar \
-        -Dnimbus.host=$nimbus \
+        -Dnimbus.host=${nimbus} \
         -Dstorm.jar=target/cta-tools-0.0.1-SNAPSHOT-storm-provided.jar \
         target/cta-tools-0.0.1-SNAPSHOT-storm-compiled.jar \
-        $process_path
+        ${process_path}
 fi
