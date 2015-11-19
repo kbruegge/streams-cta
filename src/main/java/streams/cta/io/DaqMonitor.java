a16bcc5dea9ad16062655941230a047a11623bc9
Kai Bruegge <kai.bruegge@tu-dortmund.de>
2015-11-19 11:57:19 +0100

    add a seperate daq monitor thing. add pipe xml

--- src/main/java/streams/cta/io/DaqMonitor.java
+++ src/main/java/streams/cta/io/DaqMonitor.java
@@ -20,8 +20,7 @@ import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
 
 /**
- * ProtoEventPublisher uses ZeroMQ with a Push / Pull pattern to publish telescope events serialized
- * with protocol buffer format according to DAQs data model
+ * DaqMonitor uses publishes protobufs via zeromq to the daq monitoring component
  *
  * @author kai
  */
@@ -29,10 +28,8 @@ public class DaqMonitor extends CTARawDataProcessor implements StatefulProcessor
 
     static Logger log = LoggerFactory.getLogger(DaqMonitor.class);
 
-    private ZMQ.Socket publisher;
     private ZMQ.Socket monitorPublisher;
     private ZMQ.Context context;
-    private int itemCounter = 0;
 
     private Stopwatch stopwatch;
 
@@ -40,17 +37,10 @@ public class DaqMonitor extends CTARawDataProcessor implements StatefulProcessor
     @Parameter(required = false, description = "The IP of the daq monitor. Asin in tcp://127.0.0.1:1222")
     String monitorAddress = "tcp://127.0.0.1:4849";
 
-    @Parameter(required = false)
-    String[] addresses = {"tcp://*:4849"};
 
     @Override
     public void init(ProcessContext processContext) throws Exception {
         context = ZMQ.context(1);
-        publisher = context.socket(ZMQ.PUSH);
-        for (String address : addresses) {
-            publisher.bind(address);
-            log.info("Binding to address: " + address);
-        }
 
         monitorPublisher = context.socket(ZMQ.PUB);
         monitorPublisher.bind(monitorAddress);
@@ -59,80 +49,26 @@ public class DaqMonitor extends CTARawDataProcessor implements StatefulProcessor
         stopwatch = Stopwatch.createStarted();
     }
 
-    private void sendToMonitor(int bytesSend, long ellapsedMicros){
+    private void sendToMonitor(int bytesSend, int elapsedMicros){
         CoreMessages.ThroughputStats stats = new CoreMessages.ThroughputStats();
         stats.comment = "Bytes Published";
         stats.numBytes = bytesSend;
-        stats.elapsedUs = (int) ellapsedMicros;
+        stats.elapsedUs = elapsedMicros;
         byte[] bytesTosend = CoreMessages.ThroughputStats.toByteArray(stats);
         monitorPublisher.send(bytesTosend);
     }
 
     @Override
     public Data process(Data input, CTATelescope telescope, LocalDateTime timeStamp, short[][] eventData) {
-        if (telescope.type != CTATelescopeType.LST) {
-            log.debug("Found non LST event");
-            return null;
+
+        //send stuff to the central daq monitor.
+        int elapsedMicros = (int) stopwatch.elapsed(TimeUnit.MICROSECONDS);
+        stopwatch.reset();
+        if (input.get("@packetsize") != null){
+            int packetSize = (int) input.get("@packetsize");
+            sendToMonitor(packetSize, elapsedMicros);
         }
 
-        int roi = eventData[0].length;
-        int numPixel = telescope.type.numberOfPixel;
-
-        //create the L0 data
-        CoreMessages.AnyArray anyArray = new CoreMessages.AnyArray();
-        anyArray.currentComp = CoreMessages.AnyArray.RAW;
-        anyArray.type = CoreMessages.AnyArray.S16;
-        //save samples into byte array
-        int bytesPerSample = 2;
-        ByteBuffer buffer = ByteBuffer.allocate(roi*numPixel*bytesPerSample);
-        for (int i = 0; i < numPixel; i++) {
-            for (int j = 0; j <  roi; j++) {
-                buffer.putShort(eventData[i][j]);
-            }
-        }
-        anyArray.data = buffer.array();
-
-        L0.CameraRunHeader header = new L0.CameraRunHeader();
-        header.numTraces = roi;
-        header.telescopeID = telescope.telescopeId;
-
-        L0.WaveFormData waveFormData = new L0.WaveFormData();
-        waveFormData.numSamples = roi;
-        waveFormData.samples = anyArray;
-
-        L0.PixelsChannel pixelsChannel = new L0.PixelsChannel();
-        pixelsChannel.waveforms = waveFormData;
-
-        L0.CameraEvent cameraEvent = new L0.CameraEvent();
-        cameraEvent.telescopeID = telescope.telescopeId;
-        cameraEvent.head = header;
-        cameraEvent.hiGain = pixelsChannel;
-
-        //in theory the CameraEvent can carry many payloads. In practice however. There is just one payload
-        //per package
-        byte[] payload = L0.CameraEvent.toByteArray(cameraEvent);
-        byte[][] payloadWrap = new byte[1][];
-        payloadWrap[0] = payload;
-        //create the core message:
-        CoreMessages.CTAMessage ctaMessage = new CoreMessages.CTAMessage();
-        ctaMessage.payloadType = new int[]{CoreMessages.CAMERA_EVENT};
-        ctaMessage.payloadData = payloadWrap;
-        if (input.get("@source") != null){
-            ctaMessage.sourceName = input.get("@source").toString();
-        }
-
-        byte[] bytesTosend = CoreMessages.CTAMessage.toByteArray(ctaMessage);
-
-        //send stuff to the central daq monitor every 5000 events or so
-        if(itemCounter == 5000){
-            itemCounter = 0;
-            long ellapsedMicros = stopwatch.elapsed(TimeUnit.MICROSECONDS);
-            stopwatch.reset();
-            sendToMonitor(bytesTosend.length, ellapsedMicros);
-        }
-
-        input.put("@packetSize", bytesTosend.length);
-        publisher.send(bytesTosend, 0);
         return input;
     }
 
@@ -148,17 +84,13 @@ public class DaqMonitor extends CTARawDataProcessor implements StatefulProcessor
         Thread.sleep(200);
 
 
-        if (publisher != null) {
-            publisher.close();
+        if (monitorPublisher != null) {
+            monitorPublisher.close();
         }
         if (context != null) {
             context.term();
         }
 
-    }
-
-    public void setAddresses(String[] addresses) {
-        this.addresses = addresses;
     }
 
     public void setMonitorAddress(String monitorAddress) {
