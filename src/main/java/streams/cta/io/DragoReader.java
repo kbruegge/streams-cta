035a916bb1cccc8f56ad00358c10f9c232249aaf
Kai Bruegge <kai.bruegge@tu-dortmund.de>
2016-01-13 15:30:45 +0100

    add empty class for stream

---
+++ src/main/java/streams/cta/io/DragoReader.java
@@ -0,0 +1,15 @@
+package streams.cta.io;
+
+import stream.Data;
+import stream.io.AbstractStream;
+
+/**
+ * A stream for test data from the drago module
+ * Created by Kai on 13.01.16.
+ */
+public class DragoReader extends AbstractStream{
+    @Override
+    public Data readNext() throws Exception {
+        return null;
+    }
+}
