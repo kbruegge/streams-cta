0256688ec96d3dbd6cd68cdbfd236e1c74e3d651
Kai Brügge <kai.bruegge@tu-dortmund.de>
2017-02-15 15:51:06 +0100

    add a unit test for checking whether putput of the processor is sane

--- src/main/java/streams/cta/features/Moments.java
+++ src/main/java/streams/cta/features/Moments.java
@@ -33,33 +33,20 @@ public class Moments extends CTACleanedDataProcessor{
                 size += pixel.weight;
             }
 
-            final double meanX = sumX /= size;
-            final double meanY = sumY /= size;
+            final double meanX = sumX / size;
+            final double meanY = sumY / size;
 
-//            final double cogX = meanX;
-////
-            //calculate some variances
-            double sxxx = 0, syyy = 0, sxyy = 0, sxxy = 0, sxx = 0, syy = 0, sxy = 0;
+            //calculate the covariance matrix
+            double sxx = 0, syy = 0, sxy = 0;
             for (Shower.Pixel p : shower.pixels){
                 sxx += p.weight * pow((p.xPositionInMM - meanX), 2);
                 syy += p.weight * pow((p.yPositionInMM - meanY), 2);
                 sxy += p.weight * (p.xPositionInMM - meanX) * (p.yPositionInMM - meanY);
-
-                sxxx += p.weight * pow((p.xPositionInMM - meanX), 3);
-                syyy += p.weight * pow((p.yPositionInMM - meanY), 3);
-
-                sxyy += p.weight * (p.xPositionInMM - meanX) * pow((p.yPositionInMM - meanY), 2);
-                sxxy += p.weight * (p.yPositionInMM - meanY) * pow((p.xPositionInMM - meanX), 2);
             }
 
             sxx /= size;
             syy /= size;
             sxy /= size;
-
-            sxxx /= size;
-            syyy /= size;
-            sxyy /= size;
-            sxxy /= size;
 
             //now analytically calculate the eigenvalues and vectors.
             double d0 = syy - sxx;
@@ -95,95 +82,20 @@ public class Moments extends CTACleanedDataProcessor{
             double skewness = (skewness_a/size) / pow(skewness_b/size, 3.0/2.0);
             double kurtosis = (kurtosis_a/size) / pow(kurtosis_b/size, 2);
 
+            input.put("telescope:" + telescopeId + ":shower:width", width);
+            input.put("telescope:" + telescopeId + ":shower:length", length);
+            input.put("telescope:" + telescopeId + ":shower:delta", delta);
+            input.put("telescope:" + telescopeId + ":shower:psi", delta); // i've found both names for the smae thing
+            input.put("telescope:" + telescopeId + ":shower:skewness", skewness);
+            input.put("telescope:" + telescopeId + ":shower:kurtosis", kurtosis);
+            input.put("telescope:" + telescopeId + ":shower:phi", phi);
+            input.put("telescope:" + telescopeId + ":shower:miss", miss);
+            input.put("telescope:" + telescopeId + ":shower:r", r);
+            input.put("telescope:" + telescopeId + ":shower:cog:x", meanX);
+            input.put("telescope:" + telescopeId + ":shower:cog:Y", meanY);
+
         });
 
-//        public
-//
-//    # Compute major axis line representation y = a * x + b and correlations
-//        S_xx = np.sum(image * (pix_x - mean_x) ** 2) / size
-//        S_yy = np.sum(image * (pix_y - mean_y) ** 2) / size
-//        S_xy = np.sum(image * (pix_x - mean_x) * (pix_y - mean_y)) / size
-//        S_xxx = np.sum(image * (pix_x - mean_x) ** 3) / size
-//        S_yyy = np.sum(image * (pix_y - mean_y) ** 3) / size
-//        S_xyy = np.sum(image * (pix_x - mean_x) * (pix_y - mean_y) ** 2) / size
-//        S_xxy = np.sum(image * (pix_y - mean_y) * (pix_x - mean_x) ** 2) / size
-//        S_x4 = np.sum(image * (pix_x - mean_x) ** 4) / size
-//        S_y4 = np.sum(image * (pix_y - mean_y) ** 4) / size
-//
-//    # Sanity check2:
-//
-//    # If S_xy=0 (which should happen not very often, because Size>0)
-//    # we cannot calculate Length and Width.  In reallity it is almost
-//    # impossible to have a distribution of cerenkov photons in the
-//    # used pixels which is exactly symmetric along one of the axis
-//        if S_xy == 0:
-//        raise HillasParameterizationError(("X and Y uncorrelated. Cannot "
-//        "calculate length & width"))
-//
-//        d0 = S_yy - S_xx
-//        d1 = 2 * S_xy
-//    # temp = d * d + 4 * S_xy * S_xy
-//        d2 = d0 + np.sqrt(d0 * d0 + d1 * d1)
-//        a = d2 / d1
-//    # Angle between ellipse major ax. and x-axis of camera.
-//    # Will be used for disp
-//        delta = np.arctan(a)
-//        b = mean_y - a * mean_x
-//    # Sin & Cos Will be used for calculating higher order image parameters
-//        cos_delta = 1 / np.sqrt(1 + a * a)
-//        sin_delta = a * cos_delta
-//
-//    # Compute Hillas parameters
-//         width_2 = (S_yy + a * a * S_xx - 2 * a * S_xy) / (1 + a * a)  # width squared
-//        length_2 = (S_xx + a * a * S_yy + 2 * a * S_xy) / (1 + a * a)  # length squared
-//
-//    # Sanity check3:
-//        width = 0. if width_2 < 0. else np.sqrt(width_2)
-//        length = 0. if length_2 < 0. else np.sqrt(length_2)
-//
-//        miss = np.abs(b / np.sqrt(1 + a * a))
-//        r = np.sqrt(mean_x * mean_x + mean_y * mean_y)
-//        phi = np.arctan2(mean_y, mean_x)
-//
-//    # Higher order moments
-//                sk = cos_delta * (pix_x - mean_x) + sin_delta * (pix_y - mean_y)
-//
-//        skewness = ((np.sum(image * np.power(sk, 3)) / size) /
-//                ((np.sum(image * np.power(sk, 2)) / size) ** (3. / 2)))
-//        kurtosis = ((np.sum(image * np.power(sk, 4)) / size) /
-//                ((np.sum(image * np.power(sk, 2)) / size) ** 2))
-//        asym3 = (np.power(cos_delta, 3) * S_xxx
-//                + 3.0 * np.power(cos_delta, 2) * sin_delta * S_xxy
-//                + 3.0 * cos_delta * np.power(sin_delta, 2) * S_xyy
-//                + np.power(sin_delta, 3) * S_yyy)
-//        asym = - np.power(-asym3, 1. / 3) if (asym3 < 0.) \
-//        else np.power(asym3, 1. / 3)
-//
-//        assert np.sign(skewness) == np.sign(asym)
-//
-//    # another definition of assymetry
-//    # asym = (mean_x - pix_x[np.argmax(image)]) * cos_delta
-//    #      + (mean_y - pix_y[np.argmax(image)]) * sin_delta
-//
-//    # # Compute azwidth by transforming to (p, q) coordinates
-//    # sin_theta = mean_y / r
-//    # cos_theta = mean_x / r
-//    # q = (mean_x - pix_x) * sin_theta + (pix_y - mean_y) * cos_theta
-//    # m_q = np.sum(image * q) / size
-//    # m_qq = np.sum(image * q * q) / size
-//    # azwidth_2 = m_qq - m_q * m_q
-//    # azwidth = np.sqrt(azwidth_2)
-//
-//        return MomentParameters(size=size,
-//                cen_x=mean_x*unit,
-//                cen_y=mean_y*unit,
-//                length=length*unit,
-//                width=width*unit,
-//                r=r*unit,
-//                phi=Angle(phi*u.rad),
-//                psi=Angle(delta*u.rad),
-//                miss=miss*unit,
-//                skewness=skewness,
-//                kurtosis=kurtosis)
+        return input;
     }
 }
---
+++ src/test/java/streams/cta/features/MomentsTest.java
@@ -0,0 +1,87 @@
+package streams.cta.features;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import stream.Data;
+import stream.io.SourceURL;
+import streams.cta.cleaning.TailCut;
+import streams.cta.io.ImageStream;
+import streams.hexmap.CameraGeometry;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+/**
+ * Test calulcation of some hillas parameters calculations.
+ * Created by kbruegge on 2/15/17.
+ */
+public class MomentsTest {
+
+    private ImageStream s;
+    private Moments hillas;
+    private TailCut tailCut;
+
+    @Before
+    public void setUp() throws Exception {
+        s = new ImageStream();
+        s.url = new SourceURL(CameraGeometry.class.getResource("/images.json.gz"));
+        s.init();
+
+        tailCut = new TailCut();
+        hillas = new Moments();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        s.close();
+    }
+
+
+    /**
+     * Create a stream of images. Apply the tailcut and hillas processor and check the output
+     * stored in the data item
+     */
+    @Test
+    public void testStreamWithHillas() throws Exception {
+
+        Data data = s.read();
+        while (data != null){
+            tailCut.process(data);
+            hillas.process(data);
+
+            int[]  tels = (int[]) data.get("triggered_telescopes:ids");
+            for(int id : tels){
+                assertTrue(
+                        "data item does not contain shower width",
+                        data.containsKey("telescope:" + id + ":shower:width"));
+                assertTrue(
+                        "data item does not contain shower length",
+                        data.containsKey("telescope:" + id + ":shower:length"));
+                assertTrue(
+                        "data item does not contain shower delta",
+                        data.containsKey("telescope:" + id + ":shower:delta"));
+                assertTrue(
+                        "data item does not contain shower delta",
+                        data.containsKey("telescope:" + id + ":shower:delta"));
+
+                double delta = (double) data.get("telescope:" + id + ":shower:delta");
+                double psi = (double) data.get("telescope:" + id + ":shower:psi");
+
+                assertEquals(delta, psi, 0.0);
+
+                double width = (double) data.get("telescope:" + id + ":shower:width");
+                double length = (double) data.get("telescope:" + id + ":shower:length");
+
+                //length should always be larger or equal to width because of maths!
+                assertTrue("Length hast to be greater or equal to width", length >= width);
+            }
+
+
+            data = s.read();
+        }
+
+        s.close();
+    }
+}
