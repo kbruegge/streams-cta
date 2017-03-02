e877e24aaac3f1f716afb3cfc97c6f7eb46dce45
Kai Bruegge <kai.bruegge@tu-dortmund.de>
2017-03-02 16:57:39 +0100

    start adapting to new flow

--- src/main/java/streams/cta/AddSingleTelescopePredictions.java
+++ src/main/java/streams/cta/AddSingleTelescopePredictions.java
@@ -25,50 +25,15 @@ public class AddSingleTelescopePredictions implements Processor {
 
     @Override
     public Data process(Data item) {
-        int[] triggeredTelescopes = (int[]) item.get("array:triggered_telescopes");
 
-        SummaryStatistics energyStats = new SummaryStatistics();
-        SummaryStatistics predictionStats = new SummaryStatistics();
+        double energyPrediction = regressor.applyRegression(item);
+        //signal class has name '1' I believe.
+        ProbabilityDistribution classification = classifier.applyClassifier(item);
 
-        for(int id : triggeredTelescopes) {
+        item.put("prediction:energy", energyPrediction);
+        item.put("prediction:signal", classification.getProbability("1"));
+        item.put("prediction:background", classification.getProbability("0"));
 
-            Set<String> selectedKeys = Keys.select(item, "telescope:" + id + ":*");
-            for (String key : selectedKeys) {
-                List<String> splitToList = Splitter.on(":")
-                        .trimResults()
-                        .omitEmptyStrings()
-                        .splitToList(key);
-
-                //remove first two elements (telescope:*:)
-                List<String> subList = splitToList.subList(2, splitToList.size());
-
-                String newKey = Joiner.on(":").join(subList);
-                item.put(newKey, item.get(key));
-            }
-
-            double energyPrediction = regressor.applyRegression(item);
-            //signal class has name '1' I believe.
-            ProbabilityDistribution classification = classifier.applyClassifier(item);
-
-            item.put("telescope:" + id + ":prediction:energy", energyPrediction);
-            item.put("telescope:" + id + ":prediction:signal", classification.getProbability("1"));
-            item.put("telescope:" + id + ":prediction:background", classification.getProbability("0"));
-
-            energyStats.addValue(energyPrediction);
-            predictionStats.addValue(classification.getProbability("1"));
-        }
-        Keys.select(item, "shower*").forEach(item::remove);
-
-        item.put("prediction:signal:mean", predictionStats.getMean());
-        item.put("prediction:signal:max", predictionStats.getMax());
-        item.put("prediction:signal:min", predictionStats.getMin());
-        item.put("prediction:signal:std", predictionStats.getStandardDeviation());
-
-        item.put("prediction:energy:mean",energyStats.getMean());
-        item.put("prediction:energy:max", energyStats.getMax());
-        item.put("prediction:energy:min", energyStats.getMin());
-        item.put("prediction:energy:std", energyStats.getStandardDeviation());
-        
         return item;
     }
 }
