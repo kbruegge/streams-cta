48349975c3fe104f8bc72d6ad09f78c864a91739
Alexey Egorov <alexey.egorov@tu-dortmund.de>
2017-03-02 15:40:38 +0100

    fix cleaning test

--- src/test/java/streams/cta/cleaning/CleaningTest.java
+++ src/test/java/streams/cta/cleaning/CleaningTest.java
@@ -1,8 +1,12 @@
 package streams.cta.cleaning;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
 import stream.Data;
 import stream.io.SourceURL;
+import streams.SplitByTelescope;
 import streams.cta.io.ImageStream;
 import streams.hexmap.Shower;
 
@@ -15,47 +19,62 @@ import static org.junit.Assert.assertTrue;
  */
 public class CleaningTest {
 
+    ImageStream stream;
+    TailCut tailCut;
+    SplitByTelescope split;
+    final String splitKey = "@telescopes";
+
+    @Before
+    public void setUp() throws Exception {
+        stream = new ImageStream(new SourceURL(ImageStream.class.getResource("/images.json.gz")));
+        stream.init();
+
+        split = new SplitByTelescope();
+        split.setKey(splitKey);
+        tailCut = new TailCut();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        stream.close();
+    }
 
     @Test
     public void testStreamWithTailCut() throws Exception {
-        ImageStream s = new ImageStream(new SourceURL(ImageStream.class.getResource("/images.json.gz")));
-        s.init();
-
-        TailCut tc = new TailCut();
-
-
-        Data data = s.read();
-        while (data != null){
-            tc.process(data);
-            data = s.read();
+        Data data = stream.read();
+        while (data != null) {
+            Data splitData = split.process(data);
+            for (Data tel : (Data[]) splitData.get(splitKey)) {
+                Data tailData = tailCut.process(tel);
+                assertTrue("Data item does not contain number of pixel in the shower",
+                        tailData.containsKey("shower:number_of_pixel"));
+            }
+            data = stream.read();
         }
-
-        s.close();
+        stream.close();
     }
 
 
     @Test
     public void testTailCut() throws Exception {
-        ImageStream s = new ImageStream(new SourceURL(ImageStream.class.getResource("/images.json.gz")));
-        s.init();
-
-        TailCut tc = new TailCut();
-
         //get some data and clean it
-        Data data = s.read();
-        tc.process(data);
+        Data data = stream.read();
+        Data splitData = split.process(data);
 
         //test that the shower objects are in the data item.
-        int[]  tels = (int[]) data.get("array:triggered_telescopes");
-        for(int id : tels){
-            assertTrue("data item does not contain shower", data.containsKey("telescope:" + id + ":shower"));
+        Data[] telArray = (Data[]) splitData.get(splitKey);
+        for (Data tel : telArray) {
+            tailCut.process(tel);
+            assertTrue("data item does not contain shower", tel.containsKey("shower"));
 
-            Shower shower = (Shower) data.get("telescope:" + id + ":shower");
+            Shower shower = (Shower) tel.get("shower");
 
             assertFalse("shower pixels should not be empty", shower.pixels.isEmpty());
+
+            int id = (int) tel.get("telescope:id");
             assertTrue("camera id saved in shower has to match the true id", shower.cameraId == id);
         }
 
-        s.close();
+        stream.close();
     }
 }
