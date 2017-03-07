b7c902ba93c896d3c30d625210a52ff3174a33c6
Kai Brügge <kai.bruegge@tu-dortmund.de>
2017-03-07 13:50:15 +0100

    adapt plot style

--- measurements/plot_eventrate_per_thread.py
+++ measurements/plot_eventrate_per_thread.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from natsort import natsorted
+import numpy as np
 
 
 @click.command()
@@ -20,7 +21,20 @@ def main(input_files, output_file):
         result['{}'.format(num_threads)] = datarate
 
     result = result.reindex_axis(natsorted(result.columns), axis=1)
-    sns.boxplot(result)
+
+    sns.set_style('darkgrid')
+    ax = sns.boxplot(data=result, linewidth=0.6, fliersize=1.5)
+    # ax.set_xlim([-1, 49])
+    major_ticks = np.arange(3, 49, 4)
+    minor_ticks = np.arange(0, 49, 1)
+
+    ax.set_xticks(major_ticks)
+    ax.set_xticks(minor_ticks, minor=True)
+
+    labels = np.arange(4, 49, 4)
+    ax.set_xticklabels(labels)
+
+    sns.despine()
     plt.xlabel('number of threads')
     plt.ylabel('events per second and thread')
 
