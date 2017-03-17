0bfb280bf7079c7367259ad58958acd9ea50e538
Kai Brügge <kai.bruegge@tu-dortmund.de>
2017-03-17 11:34:31 +0100

    cleanup interface

--- src/test/resources/test_interface.py
+++ src/test/resources/test_interface.py
@@ -1,5 +1,4 @@
 import Pyro4
-# import sys
 
 
 @Pyro4.expose
@@ -13,19 +12,6 @@ class Processor(object):
 
     def add(self, a, b):
         return a + b
-
-
-# daemon = Pyro4.Daemon()
-# uri = daemon.register(Processor)
-#
-# # print the uri so we can use it in the client later
-# print('{},{},{}\n'.format(uri.object, uri.host, uri.port))
-#
-# # flush the output of print. because we read that output in the java thread.
-# # and it wont get printed otherwise.
-# sys.stdout.flush()
-# # start the event loop of the server to wait for calls from java
-# daemon.requestLoop()
 
 
 def main():
