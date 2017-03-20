96feda90cecc4ca2f3a3016b40815e3ca7ed8fbc
Kai Brügge <kai.bruegge@tu-dortmund.de>
2017-03-20 13:03:23 +0100

    check wether the process has been started

--- src/main/java/streams/PythonBridge.java
+++ src/main/java/streams/PythonBridge.java
@@ -2,8 +2,12 @@ package streams;
 
 import net.razorvine.pyro.NameServerProxy;
 import net.razorvine.pyro.PyroProxy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * This object is the heart of the connection between python and java.
@@ -14,29 +18,76 @@ import java.io.IOException;
  * Created by kbruegge on 3/16/17.
  */
 public class PythonBridge implements AutoCloseable {
+    private Logger log = LoggerFactory.getLogger(PythonBridge.class);
 
-    private final Process nameServerProcess;
-    private final Process pythonProcess;
+    private Process nameServerProcess = null;
+    private Process pythonProcess = null;
     private final PyroProxy remoteObject;
 
+    /**
+     * The PythonBridge starts a pyro name server by invoking  "python -m Pyro4.naming"
+     * using javas ProcessBuilder. The output of that process is parsed for the string "NS Running"
+     * until a timeout is reached.
+     * After the name server has been started, the actual python script is run.
+     * The python script is started in the same manner.
+     * The python script has to start the pyro daemon and must not redirect its output to somewhere else.
+     *
+     * @param pathToPythonScript the path to the python script with the exposed pyro methods
+     * @throws IOException in case an error occurs when starting the processes
+     */
+    public PythonBridge(String pathToPythonScript) throws IOException {
+        try {
+            String[] nameServerCommand = {"python", "-u", "-m", "Pyro4.naming"}; // -u for unbuffered python output
 
-    public PythonBridge(String pathToPythonScript) throws IOException, InterruptedException {
-        String[] nameServerCommand = {"python", "-m", "Pyro4.naming"};
-        ProcessBuilder pB = new ProcessBuilder(nameServerCommand);
-        nameServerProcess = pB.start();
+            nameServerProcess = new ProcessBuilder(nameServerCommand).start();
+            BufferedReader stdin = new BufferedReader(new InputStreamReader(nameServerProcess.getInputStream()));
 
-        //TODO: find a way to check whether this has finished loading instead of sleeping
-        Thread.sleep(2000);
+            boolean isNameServerRunning = false;
+            for (int i = 0; i < 10; i++) {
+                String output = stdin.readLine();
+                if (output != null && output.startsWith("NS running")) {
+                    log.debug("NameServer is running");
+                    isNameServerRunning = true;
+                    break;
+                }
+                Thread.sleep(200);
+            }
+            if (!isNameServerRunning) {
+                nameServerProcess.destroy();
+                throw new IOException("Timeout while waiting for start of NameServer.");
+            }
 
 
-        String[] command = {"python", pathToPythonScript};
-        pythonProcess = new ProcessBuilder(command).start();
+            String[] command = {"python", "-u", pathToPythonScript}; // -u for unbuffered python output
 
-        Thread.sleep(2000);
+            pythonProcess = new ProcessBuilder(command).start();
+            stdin = new BufferedReader(new InputStreamReader(pythonProcess.getInputStream()));
 
-        NameServerProxy nsProxy = NameServerProxy.locateNS("localhost");
+            boolean isPyroDaemonRunning = false;
+            for (int i = 0; i < 10; i++) {
+                String output = stdin.readLine();
+                if (output != null && output.startsWith("Pyro daemon running.")) {
+                    log.debug("Pyro daemon is running.");
+                    isPyroDaemonRunning = true;
+                    break;
+                }
+                Thread.sleep(200);
+            }
+            if (!isPyroDaemonRunning) {
+                pythonProcess.destroy();
+                throw new IOException("Timeout while waiting for start of PyroDaemon.");
+            }
 
-        remoteObject = new PyroProxy(nsProxy.lookup("streams.processors"));
+
+            NameServerProxy nsProxy = NameServerProxy.locateNS("localhost");
+
+            remoteObject = new PyroProxy(nsProxy.lookup("streams.processors"));
+        } catch (IOException | InterruptedException e){
+            pythonProcess.destroyForcibly();
+            nameServerProcess.destroyForcibly();
+            throw new IOException("An error occured while invoking the python processes");
+        }
+
     }
 
     private void stop(){
@@ -45,6 +96,15 @@ public class PythonBridge implements AutoCloseable {
         pythonProcess.destroy();
     }
 
+    /**
+     * Calls a Python method by name.
+     * This methods delegates to the {@link PyroProxy#call} method.
+     * 
+     * @param name the name of the python method to call
+     * @param args the argumeents to pass to python
+     * @return the return value of the python method. The object has to be cast back to the proper type.
+     * @throws IOException in case an error occurs when calling the python method.
+     */
     public Object callMethod(String name, Object... args) throws IOException {
         return remoteObject.call(name, args);
     }
