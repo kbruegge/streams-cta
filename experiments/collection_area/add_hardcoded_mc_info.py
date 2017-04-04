9388478d6e54fd6d3b9e2a8669803a21776419ad
Kai Brügge <kai.bruegge@tu-dortmund.de>
2017-04-04 11:24:53 +0200

    add collection area to mc info

--- experiments/collection_area/add_hardcoded_mc_info.py
+++ experiments/collection_area/add_hardcoded_mc_info.py
@@ -24,9 +24,8 @@ def main(input_folder, output_file):
     gammas = filter(lambda f: f.startswith('gamma'), map(
         lambda s: s.replace('.json.gz', ''),  os.listdir(input_folder)))
 
-    gammas = {s: {'index': -2.0, 'simulated_showers': 20000*10} for s in gammas}
-    protons = {s: {'index': -2.0, 'simulated_showers': 50000*20}
-               for s in protons}
+    gammas = {s: {'index': -2.0, 'simulated_showers': 20000*10, 'scatter_radius_meter': 2500.0} for s in gammas}
+    protons = {s: {'index': -2.0, 'simulated_showers': 50000*20, 'scatter_radius_meter': 3000.0} for s in protons}
 
     entries = dict(gammas, **protons)
     df = pd.DataFrame.from_dict(entries, orient='index')
