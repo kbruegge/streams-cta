87ab546f5153f15fd896c282edafa12d3dbfb89b
Kai Brügge <kai.bruegge@tu-dortmund.de>
2017-04-07 17:31:22 +0200

    make nice specgtrum objects

--- experiments/sensitivity/power_law.py
+++ experiments/sensitivity/power_law.py
@@ -1,5 +1,6 @@
 import numpy as np
 import astropy.units as u
+from scipy.integrate import quad
 
 
 @u.quantity_input(e_min=u.TeV, e_max=u.TeV, energy=u.TeV)
@@ -10,58 +11,83 @@ def power_law(
         e_max=300*u.TeV,
         index=-2.0,
         collection_area=np.pi*3000**2 * u.m**2,
-        simulation_time=1*u.second,
         ):
     total = integral_of_power_spectrum(e_min.to('TeV').value, e_max.to('TeV').value, index)
-    return ((N / total) * (energy/u.TeV)**index) / (u.TeV * collection_area * simulation_time)
+    return ((N / total) * (energy/u.TeV)**index) / (u.TeV * collection_area)
 
 
-@u.quantity_input(energy=u.TeV)
-def crab_source_rate(energy):
-    ''' function for a pseudo-Crab point-source rate
-    Crab source rate:   dN/dE = 3e-7  * (E/TeV)**-2.48 / (TeV * m² * s)
-    (watch out: unbroken power law... not really true)
-    norm and spectral index reverse engineered from HESS plot...
+class Spectrum():
 
-    Parameters
-    ----------
-    energy : astropy quantity
-        energy for which the rate is desired
+    def _f(self, e, area, t_obs):
+        return (self.rate(e*u.TeV) * area.to('m^2') * t_obs.to('s')).value
 
-    Returns
-    -------
-    flux : astropy quantity
-        differential flux at E
+    @u.quantity_input(e_min=u.TeV, e_max=u.TeV, area=u.m**2, t_obs=u.s)
+    def expected_events(self, e_min, e_max, area, t_obs):
+        a = e_min.to('TeV').value
+        b = e_max.to('TeV').value
 
-    '''
-    return 3e-7 * (energy/u.TeV)**-2.48 / (u.TeV * u.m**2 * u.s)
+        events, _ = quad(lambda e: self._f(e, area, t_obs), a, b)
+        return events
+
+    @u.quantity_input(energies=u.TeV)
+    def event_weights(self, energies, simulated_showers, expected_showers, index_mc):
+        ew = (energies/u.TeV)**(np.abs(index_mc) - np.abs(self.index))
+        return (ew/np.sum(ew)) * expected_showers/simulated_showers
 
 
-@u.quantity_input(energy=u.TeV)
-def CR_background_rate(energy):
-    ''' function of the cosmic ray spectrum (simple power law, no knee/ankle)
-    Cosmic Ray background rate: dN/dE = 0.215 * (E/TeV)**-8./3 / (TeV * m² * s * sr)
-    norm and spectral index reverse engineered from "random" CR plot...
+class CrabSpectrum(Spectrum):
+    index = -2.48
 
-    Parameters
-    ----------
-    energy : astropy quantity
-        energy for which the rate is desired
+    @u.quantity_input(energy=u.TeV)
+    def rate(self, energy):
+        ''' function for a pseudo-Crab point-source rate
+        Crab source rate:   dN/dE = 3e-7  * (E/TeV)**-2.48 / (TeV * m² * s)
+        (watch out: unbroken power law... not really true)
+        norm and spectral index reverse engineered from HESS plot...
 
-    Returns
-    -------
-    flux : astropy quantity
-        differential flux at E
+        Parameters
+        ----------
+        energy : astropy quantity
+            energy for which the rate is desired
 
-    '''
-    return 100 * 0.1**(8./3) * (energy/u.TeV)**(-8./3) / (u.TeV * u.m**2 * u.s * u.sr)
+        Returns
+        -------
+        flux : astropy quantity
+            differential flux at E
+
+        '''
+        return 3e-7 * (energy/u.TeV)**-2.48 / (u.TeV * u.m**2 * u.s)
 
 
+class CosmicRaySpectrum(Spectrum):
+    index = -8.0/3.0
+
+    @u.quantity_input(energy=u.TeV)
+    def rate(self, energy):
+        ''' function of the cosmic ray spectrum (simple power law, no knee/ankle)
+        Cosmic Ray background rate: dN/dE = 0.215 * (E/TeV)**-8./3 / (TeV * m² * s * sr)
+        norm and spectral index reverse engineered from "random" CR plot...
+
+        Parameters
+        ----------
+        energy : astropy quantity
+            energy for which the rate is desired
+
+        Returns
+        -------
+        flux : astropy quantity
+            differential flux at E
+
+        '''
+        return 100 * 0.1**(8./3) * (energy/u.TeV)**(-8./3) / (u.TeV * u.m**2 * u.s * u.sr)
+
+    def expected_events(self, e_min, e_max, area, t_obs):
+        e = super(CosmicRaySpectrum, self).expected_events(e_min, e_max, area, t_obs) * (1-np.cos((6*u.deg).to('rad')))*2 * np.pi
+        return e.value
 
 
-def integral_of_power_spectrum(e_low, e_high, index):
-    return (1 / (index + 1)) * e_high**(index + 1) - \
-        (1 / (index + 1)) * e_low**(index + 1)
+def integral_of_power_spectrum(e_low, e_high, index, N=1):
+    return N * ((1 / (index + 1)) * e_high**(index + 1) - (1 / (index + 1)) * e_low**(index + 1) )
 
 
 @u.quantity_input(event_energies=u.GeV, e_min=u.GeV, e_max=u.GeV)
