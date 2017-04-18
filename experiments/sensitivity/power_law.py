dcba377359c1c00af70e150d456cf9467ab7c9dc
Kai Brügge <kai.bruegge@tu-dortmund.de>
2017-04-18 14:11:05 +0200

    power law with proper event weights

--- experiments/sensitivity/power_law.py
+++ experiments/sensitivity/power_law.py
@@ -5,12 +5,15 @@ import astropy.units as u
 class Spectrum():
 
     @u.quantity_input(e_min=u.TeV, e_max=u.TeV,)
-    def draw_energy_distirbution(self, e_min, e_max, shape):
-        a = e_min.to('TeV').value**self.index
-        b = e_max.to('TeV').value**self.index
-        r = np.random.uniform(a, b, shape)
-        g = self.index
-        e = (r * (g + 1)/self.normalization_constant)**(1.0/(g+1))
+    def draw_energy_distribution(self, e_min, e_max, shape, index=None):
+        if not index:
+            index = self.index
+
+        a = e_min.to('TeV').value**(index + 1)
+        b = e_max.to('TeV').value**(index + 1)
+        r = np.random.uniform(0, 1, shape)
+        k = (a + (b - a)*r)
+        e = k**(1./(index + 1))
         return e*u.TeV
 
     @u.quantity_input(energy=u.TeV)
@@ -69,20 +72,6 @@ class Spectrum():
         events = np.array(events)
         return events, edges
 
-    @u.quantity_input(e_min=u.TeV, e_max=u.TeV)
-    def _correction_factor(self, e_min, e_max, simulated_index=-2.0):
-        index = self.index
-
-        e_min = e_min.to('TeV')/u.TeV
-        e_max = e_max.to('TeV')/u.TeV
-
-        k = (1 + index)/(1 + simulated_index)
-        events_simulated = (e_max**(1 + simulated_index) - e_min**(1. + simulated_index))
-        events_from_spectrum = (e_max**(1 + index) - e_min**(1. + index))
-
-        e_0 = k * events_simulated / events_from_spectrum
-        return e_0 * u.TeV
-
     @u.quantity_input(
         event_energies=u.TeV,
         e_max_simulated=u.TeV,
@@ -100,15 +89,22 @@ class Spectrum():
             ):
         event_energies = event_energies.to('TeV')
 
-        e_0 = self._correction_factor(e_min_simulated, e_max_simulated, simulated_index)
+        expected_events = self.expected_events(
+                    e_min_simulated,
+                    e_max_simulated,
+                    area,
+                    t_obs=1*u.s
+                )
 
-        rate = self.expected_events(e_min_simulated, e_max_simulated, area, t_obs=1*u.s)\
-            / u.s
+        w = (event_energies/u.TeV)**(self.index - simulated_index)
+        k = (len(event_energies)/simulated_showers)*expected_events
+        w = k * w / np.sum(w)
 
-        w = (event_energies / e_0)**(self.index - simulated_index) \
-            * rate / simulated_showers
+        # print('expected_events {}'.format(expected_events))
+        # print('sum of weights: {}'.format(np.sum(w)))
 
-        return w.to(1/u.s)
+        assert w.si.unit.is_unity() == True
+        return w.value
 
 
 class CrabSpectrum(Spectrum):
@@ -121,3 +117,39 @@ class CosmicRaySpectrum(Spectrum):
     index = -2.7
     normalization_constant = 9.6e-9 / (u.GeV * u.cm**2 * u.s * u.sr)
     generator_solid_angle = 6 * u.deg
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    s = CrabSpectrum()
+
+    e_min = 0.1*u.TeV
+    e_max = 100*u.TeV
+    area = 100*u.km**2
+    N = 1000000
+    simulation_index = -1.8
+
+    random_energies = s.draw_energy_distribution(e_min, e_max, N, index=simulation_index)
+
+    events, edges = s.expected_events_for_bins(e_min=e_min, e_max=e_max, area=area, t_obs=1*u.s, bins=20)
+
+    bin_center = 0.5 * (edges[:-1] + edges[1:])
+    bin_width = np.diff(edges)
+
+    plt.errorbar(
+            bin_center.value,
+            events,
+            xerr=bin_width.value*0.5,
+            linestyle='',
+            marker='.',
+            label='expected events from crab'
+        )
+    plt.hist(random_energies, bins=edges, histtype='step')
+
+    w = s.weight(random_energies, e_min, e_max, area=area, simulated_showers=N, simulated_index=simulation_index)
+    plt.hist(random_energies, bins=edges, histtype='step', weights=w, label='reweighted energies')
+
+    plt.yscale('log')
+    plt.xscale('log')
+    plt.legend()
+    plt.show()
